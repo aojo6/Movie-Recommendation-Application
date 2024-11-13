8edd1a5e3b0b427cc35c44a7b3c9af4b66b3e410
Dhruv249 <dhruvpatel0713@gmail.com>
2024-11-13 14:23:06 -0500

    Update home.component.ts

--- movierecommendation/src/app/home/home.component.ts
+++ movierecommendation/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationMenuComponent } from '../navigation-menu/navigation-menu.component';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavigationMenuComponent],
+  imports: [NavigationMenuComponent, MatMenuModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
